edbec5636f1ad2610c85a723297212638b0a8dc6
Rajinder Yadav <devguy.ca@gmail.com>
2018-07-14 16:34:51 -0400

    Initial Web site setup.

---
+++ idea.md
@@ -0,0 +1,21 @@
+# ECX
+
+Main Image
+
+Enterprise Cloud Experts
+
+Worry free Development and Infrastructure Service
+
+Infrastructure Solutions
+
+Seamlessly build, deploy and move to new servers or the Cloud. Eliminate the headaches and cost by having a frictionless environment done correctly from the start.
+
+Have an existing infrastructure already, then talk to us about moderanizing your network or moving it to the cloud.
+
+Reduce Cost
+Move Faster
+Work Smarter
+
+Modernize Your Software Development Life-cycle
+
+Develope -> Test -> Build -> Deploy -> Monitor -> Scale
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: 'main', component: HomeComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
